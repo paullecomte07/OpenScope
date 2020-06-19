d18ade26891e19d9f9ea91c4319a09a7ee8d36a6
paullecomte07 <42438530+paullecomte07@users.noreply.github.com>
2020-06-20 01:11:28 +0200

    Update app.py

--- OpenScope_Software/app.py
+++ OpenScope_Software/app.py
@@ -1,4 +1,5 @@
-/***************************************************************************
+'''
+***************************************************************************
   Copyright 2020 Paul Lecomte
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -11,6 +12,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.
 ***************************************************************************/
+'''
 
 from importlib import import_module
 import os
